8dec32564a4996a9ff34f29c26798b094fb189ca
Marco Rodella <m.rodella@invisiblefarm.com>
2025-03-08 11:11:58 +0100

    ✨ Complete PrimeService for number < 2

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -2,9 +2,17 @@
 
 public class PrimeService
 {
+    // Un numero primo è un numero divisibile solo per 1 e per se stesso.
+    // Nella definizione del dizionario, un numero primo è “un numero naturale, maggiore di 1, che non può essere rappresentato
+    // come prodotto di due numeri naturali più piccoli di lui”.
+    //
+    // I numeri primi sono i mattoni della matematica. Un numero primo è un numero intero maggiore di 1 che può essere
+    // diviso solo per 1 e per se stesso. In altre parole, quando provi a dividere un numero primo per qualsiasi altro numero,
+    // otterrai sempre un resto. Qualsiasi numero naturale può essere assemblato da numeri primi tramite moltiplicazione.
+    
     public bool IsPrime(int number)
     {
-        if (number == 1) return false;
+        if (number < 2) return false;
         
         throw new NotImplementedException("Not fully implemented.");
     }
