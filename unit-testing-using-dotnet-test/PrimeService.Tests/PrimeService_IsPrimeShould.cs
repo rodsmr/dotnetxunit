d7df0c195495f671aeed58c4090ac8870b3fa521
Marco Rodella <m.rodella@invisiblefarm.com>
2025-03-08 11:38:13 +0100

    🐲 Add other tests

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -73,4 +73,82 @@ public class PrimeService_IsPrimeShould
         // Assert
         Assert.True(result, $"3 should be prime");
     }
+
+    [Fact]
+    public void IsPrime_IsInput4_ReturnFalse()
+    {
+        // Arrange: skip
+        
+        // Act
+        var result = _primeService.IsPrime(4);
+        
+        // Assert
+        Assert.False(result, $"4 should NOT be prime");
+    }
+    
+    // Add more tests for prime numbers using Theory
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(7)]
+    public void IsPrime_InputLessThan10_ReturnTrue(int number)
+    {
+        // Arrange: skip
+        
+        // Act
+        bool result = _primeService.IsPrime(number);
+        
+        // Assert
+        Assert.True(result, $"{number} should be prime");
+    }
+    
+    [Theory]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(9)]
+    public void IsPrime_InputLessThan10_ReturnFalse(int number)
+    {
+        // Arrange: skip
+        
+        // Act
+        bool result = _primeService.IsPrime(number);
+        
+        // Assert
+        Assert.False(result, $"{number} should NOT be prime");
+    }
+    
+    [Theory]
+    [InlineData(11)]
+    [InlineData(13)]
+    [InlineData(17)]
+    [InlineData(19)]
+    public void IsPrime_InputGreatherThan10_ReturnTrue(int number)
+    {
+        // Arrange: skip
+        
+        // Act
+        bool result = _primeService.IsPrime(number);
+        
+        // Assert
+        Assert.True(result, $"{number} should be prime");
+    }
+    
+    [Theory]
+    [InlineData(12)]
+    [InlineData(14)]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(18)]
+    public void IsPrime_InputGreatherThan10_ReturnFalse(int number)
+    {
+        // Arrange: skip
+        
+        // Act
+        bool result = _primeService.IsPrime(number);
+        
+        // Assert
+        Assert.False(result, $"{number} should be prime");
+    }
 }
