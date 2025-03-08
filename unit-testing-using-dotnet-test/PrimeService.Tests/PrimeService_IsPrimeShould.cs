dd8486105e97f2b358e95ada579f1fc6118f7574
Marco Rodella <m.rodella@invisiblefarm.com>
2025-03-08 11:02:16 +0100

    ✨ Create common primeservice (is it Mock?)

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -2,16 +2,50 @@
 
 public class PrimeService_IsPrimeShould
 {
+    private readonly PrimeService _primeService = new();
+
     [Fact]
     public void IsPrime_InputIs1_ReturnFalse()
     {
-        // Arrange
-        var primeService = new PrimeService();
+        // Arrange: replaced by the constructor
+        // var primeService = new PrimeService();
         
         // Act
-        bool result = primeService.IsPrime(1);
+        bool result = _primeService.IsPrime(1);
         
         // Assert
         Assert.False(result, "1 should not be prime");
+    }
+    
+    // Bad practice create a new test if only a parameter is changed
+    // [Fact]
+    // public void IsPrime_InputIs0_ReturnFalse()
+    // {
+    //     // Arrange
+    //     var primeService = new PrimeService();
+    //     
+    //     // Act
+    //     bool result = primeService.IsPrime(0);
+    //     
+    //     // Assert
+    //     Assert.False(result, "0 should not be prime");
+    // }
+    
+    // Test for a prime number with input: -1, 0, 1
+    // Use Theory insted of Fact
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+    {
+        // Arrange: replaced by the constructor
+        // var primeService = new PrimeService();
+        
+        // Act
+        bool result = _primeService.IsPrime(value);
+        
+        // Assert
+        Assert.False(result, $"{value} should not be prime");
     }
 }
